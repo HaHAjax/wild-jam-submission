710a115df432e26f893faa97686347b40515c098
dapenguinlord <dapenguinlord25@gmail.com>
2025-06-13 13:15:51 -1000

    triangulator is now looking for specific objective instead of closest

--- scenes/triangulator/triangulator.gd
+++ scenes/triangulator/triangulator.gd
@@ -2,17 +2,32 @@ class_name Triangulator
 extends Node2D
 
 var target : Objective
-
+var current_target : String
 
 func _init() -> void:
 	add_to_group("triangulator")
 
 
+func _ready() -> void:
+	current_target = "first" #TODO: remove this when we add the target menu
+
+
 func _physics_process(_delta: float) -> void:
-	target = get_closest_objective()
-	print(target)
+	print(get_distance_to_objective())
 
 
+func change_target(target_string : String) -> void:
+	current_target = target_string
+
+
+func get_distance_to_objective() -> float:
+	var target : Objective = get_tree().get_first_node_in_group(current_target)
+	var dist : float = global_position.distance_to(target.global_position)
+	
+	return dist
+
+
+# TODO: remove if we don't use this function
 func get_closest_objective() -> Objective:
 	
 	var objectives : Array[Objective]
